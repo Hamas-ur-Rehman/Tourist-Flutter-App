50c42bd9e41a789e35db4cacbbe9ca4f1e61a7cf
Hamas Ur Rehman <47780362+Hamas-ur-Rehman@users.noreply.github.com>
2022-02-18 22:12:34 +0500

    profile overhaul

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -51,67 +51,149 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(
-            height: 30,
-          ),
-          Text(
-            name ?? widget.namex,
-            style: TextStyle(fontSize: 20),
-          ),
-          Text(
-            email ?? widget.emailx,
-            style: TextStyle(fontSize: 20),
-          ),
-          Text(
-            isAdmin == true || widget.adminx
-                ? 'Business account'
-                : 'Tourist account',
-            style: TextStyle(fontSize: 20),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Material(
-              borderRadius: BorderRadius.circular(14.0),
-              elevation: 0,
-              child: Container(
-                height: 56,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(14.0),
-                ),
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    onTap: () {
-                      removePrefs();
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const MyApp(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      splashColor: primaryBlue,
+                      splashRadius: 10,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        size: 30,
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Profile Info',
+                        style: heading2.copyWith(color: textBlack),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Image.asset(
+                        'assets/images/accent.png',
+                        width: 99,
+                        height: 4,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Name',
+                          style: heading6.copyWith(color: textBlack),
                         ),
-                        (route) => false,
-                      );
-                    },
-                    borderRadius: BorderRadius.circular(14.0),
-                    child: Center(
-                      child: Text(
-                        "Log out",
-                        style: heading5.copyWith(color: Colors.white),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          name ?? widget.namex,
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.grey[700]),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Email',
+                          style: heading6.copyWith(color: textBlack),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          email ?? widget.emailx,
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.grey[700]),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Admin',
+                          style: heading6.copyWith(color: textBlack),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          isAdmin == true || widget.adminx
+                              ? 'Business account'
+                              : 'Tourist account',
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.grey[700]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Material(
+                  borderRadius: BorderRadius.circular(14.0),
+                  elevation: 0,
+                  child: Container(
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () {
+                          removePrefs();
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => const MyApp(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                        borderRadius: BorderRadius.circular(14.0),
+                        child: Center(
+                          child: Text(
+                            "Log out",
+                            style: heading5.copyWith(color: Colors.white),
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
